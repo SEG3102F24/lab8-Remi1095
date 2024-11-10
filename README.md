be6897d6a3f66c175ec92c9cd7a9d1175c71f658
Remi1095 <remi.grosjean1@gmail.com>
2024-11-10 17:11:00 -0500

    finished

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # employeeGql
+
+| Student        | Number    |
+| -------------- | --------- |
+| Rémi Gros-Jean | 300258238 |
+
+# MongoDB
+```
+docker run -it -p 27017:27017 --name mongodb mongo
+```
--- src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
+++ src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
@@ -1,8 +1,49 @@
 package seg3x02.employeeGql.resolvers
 
+import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.graphql.data.method.annotation.MutationMapping
+import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.stereotype.Controller
+import seg3x02.employeeGql.entity.Employee
+import seg3x02.employeeGql.repository.EmployeesRepository
+import seg3x02.employeeGql.resolvers.types.CreateEmployeeInput
+import java.util.UUID
 
 @Controller
-class EmployeesResolver {
+class EmployeesResolver(
+    private val employeesRepository: EmployeesRepository
+) {
 
+    // Query to get all employees
+    @QueryMapping
+    fun employees(): List<Employee> {
+        return employeesRepository.findAll()
+    }
+
+    // Mutation to add a new employee
+    @MutationMapping
+    fun newEmployee(@Argument("createEmployeeInput") input: CreateEmployeeInput): Employee {
+        // Validate the input before proceeding
+        if (input.name != null &&
+            input.dateOfBirth != null &&
+            input.city != null &&
+            input.salary != null &&
+            input.gender != null &&
+            input.email != null) {
+
+            val employee = Employee(
+                name = input.name,
+                dateOfBirth = input.dateOfBirth,
+                city = input.city,
+                salary = input.salary,
+                gender = input.gender,
+                email = input.email
+            )
+
+            employeesRepository.save(employee) // Save the new employee to the repository
+            return employee
+        } else {
+            throw Exception("Invalid input")
+        }
+    }
 }
